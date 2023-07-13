d80c532c7bf8c0d1aa08dccb24d7a80a72129a31
Giovanni Quevedo <giovanni.quevedo5@gmail.com>
2023-07-13 10:16:55 -0700

    Removed pesky API key so sniffers like you can get lost ;)

--- kabudle/src/pages/Add.tsx
+++ kabudle/src/pages/Add.tsx
@@ -5,19 +5,17 @@ import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { useAuthState } from "react-firebase-hooks/auth";
 import React from "react";
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD0KzKi2Ry7Xn5m3OdU0HbuP3fe8TZTlOs",
+  apiKey: process.env.REACT_APP_API_KEY,
   authDomain: "kabudle.firebaseapp.com",
   projectId: "kabudle",
   storageBucket: "kabudle.appspot.com",
   messagingSenderId: "515753945489",
-  appId: "1:515753945489:web:cfaa13ee3fa3dc394ad230",
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 // Initialize Firebase
@@ -27,7 +25,8 @@ function Add() {
   return (
     <div>
       <h1>You + Me is a hell of a recipe ~ 3 ~ : </h1>
-      <p>{process.env.REACT_APP_FB_API}</p>
+      <p>{process.env.REACT_APP_API_KEY}</p>
+      <p>{process.env.REACT_APP_APP_ID}</p>
       <a href="/">Home</a>
       <br></br>
       <a href="/favorites">Favorites</a>
